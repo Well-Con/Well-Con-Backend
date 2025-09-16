a47b83a49ffc415ec85f6d68985f8fbe81bdab80
KshitijGupta99 <guptakshitij111@gmail.com>
2025-09-16 22:34:34 +0530

    feat: update Prisma client initialization and enhance routing by adding DoctorRouter

--- src/prisma.ts
+++ src/prisma.ts
@@ -3,8 +3,9 @@ import { PrismaClient } from "@prisma/client";
 const _prisma = new PrismaClient();
 
 async function main() {
-  const user = await _prisma.user.create({ data: { name: "John Doe" } });
-  console.log(user);
+  // const user = await _prisma.user.create({ data: { name: "John Doe", email: "john.doe@example.com" } });
+  // console.log(user);
+  console.log("Prisma Client is connected and ready.");
     // console.log(Object.keys(_prisma))
 
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import _prisma from "@prisma/client";
 import AuthRouter from "./auth.routes";
+import DoctorRouter from "./doctor.routes";
 
 const v1Router = express.Router();
 
 v1Router.use("/auth", AuthRouter);
+v1Router.use("/doctor", DoctorRouter);
 // v1Router.use("/users", UserRouter);
 // v1Router.use("/google", GoogleOAuthRouter);
 

088ed292058d0e355a5cf151eb049f5202f74a57
KshitijGupta99 <guptakshitij111@gmail.com>
2025-09-16 22:10:14 +0530

    feat: configure CORS options to allow specific origins and enable credentials

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,16 @@ morgan.token("colored-status", (req, res) => {
 morgan.token("iso-date", () => new Date().toISOString());
 
 app.use(helmet());
+const corsOptions = {
+    origin: ['http://localhost:5173',
+        "http://localhost:19006", // Expo Go
+        "exp://127.0.0.1:19000",  // If using Expo Dev Client
+        "http://localhost:3000",
+    ],
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.use(
   morgan((tokens, req, res) => {
